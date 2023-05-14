f60216a4d386463e610cca471c2434edd312df3f
PB-Rockz <82457379+PB-Rockz@users.noreply.github.com>
2023-05-14 21:09:06 +0530

    .

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://edith-store-pb-rockz.vercel.app/";
+  : "https://edith-store.vercel.app/";
